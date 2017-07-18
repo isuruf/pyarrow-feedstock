54df246e44960a1db7ad805da250700644c45ef0
Isuru Fernando <isuruf@gmail.com>
2017-07-18 22:31:43 +0530

    Test

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ set -x
 export ARROW_HOME=$PREFIX
 export PARQUET_HOME=$PREFIX
 
+export CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
+
 cd python
 $PYTHON setup.py \
         build_ext --build-type=release --with-parquet \
